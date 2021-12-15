a28c8963be56e4aa47c646a75483a822f5c679d0
Drew Pate <andrewcpate@gmail.com>
2021-12-14 20:39:05 -0500

    restructure files

---
+++ public/right.js
@@ -0,0 +1,10 @@
+function Right(props){
+    return (<>
+        <right>
+            <PlusMinus section="right" handle={props.handle}/>
+            <div className="section">Right:{props.data.right}</div>
+            <Data data={props.data}/>
+        </right>
+        </>);
+
+}
